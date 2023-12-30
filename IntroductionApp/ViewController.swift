ec25a81f719a02149594aa0964a31e981edc2282
Li Yuanhe <yuanhe.li@vanderbilt.edu>
2023-12-29 18:14:29 -0600

    finished app

--- IntroductionApp/ViewController.swift
+++ IntroductionApp/ViewController.swift
@@ -2,13 +2,47 @@
 //  ViewController.swift
 //  IntroductionApp
 //
-//  Created by Jay's Mac Air on 12/29/23.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var schoolNameTextField: UITextField!
+    
+    @IBOutlet weak var yearSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var numberOfPetsLabel: UILabel!
+    
+    @IBOutlet weak var morePetsStepper: UIStepper!
+    
+    @IBOutlet weak var morePetsSwitch: UISwitch!
+    
+    @IBAction func stepperDidChange(_ sender: UIStepper) {
+        numberOfPetsLabel.text = "\(Int(sender.value))"
+    }
+    
+    @IBAction func introduceSelfDidTapped(_ sender: UIButton) {
+        // Chooses the title we selected in segmented control.
+        let year = yearSegmentedControl.titleForSegment(at: yearSegmentedControl.selectedSegmentIndex)
+        
+        let introduction = "My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend \(schoolNameTextField.text!). I am currently in my \(year!) year of school and I own \(numberOfPetsLabel.text!) dogs. It is \(morePetsSwitch.isOn) that I want more pets."
+        
+        // create that alert.
+        let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
+        
+        // the section after the alert.
+        let action = UIAlertAction(title: "Nice to meet you!", style: .default, handler: nil)
+        
+        // add the action using controller.
+        alertController.addAction(action)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
